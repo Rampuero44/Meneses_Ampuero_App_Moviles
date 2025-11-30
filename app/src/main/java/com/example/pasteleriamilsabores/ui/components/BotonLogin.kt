ba5a1352cd1b0924d5a67d6dfac2dcd263d986a0
Joaquin <jo.menesesl@duocuc.cl>
2025-11-30 18:49:27 -0300

    beta

--- app/src/main/java/com/example/pasteleriamilsabores/ui/components/BotonLogin.kt
+++ app/src/main/java/com/example/pasteleriamilsabores/ui/components/BotonLogin.kt
@@ -1,4 +1,4 @@
-package com.example.pasteleriamilsabores.ui.components
+    package com.example.pasteleriamilsabores.ui.components
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
